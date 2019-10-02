15ef83dbb270f7d876cacf6751978aefdf4092a9
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 16:12:44 +0300

    Pickers in Form

--- PickersInForm/ContentView.swift
+++ PickersInForm/ContentView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    var strengths = ["Mild", "Medium", "Mature"]
+
+    @State private var selectedStrength = 0
+
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section {
+                    Picker(selection: $selectedStrength, label: Text("Strength")) {
+                        ForEach(0 ..< strengths.count) {
+                            Text(self.strengths[$0])
+
+                        }
+                    }
+                }
+            }.navigationBarTitle("Select your cheese")
+
+        }
     }
 }
 
